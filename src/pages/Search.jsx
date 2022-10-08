0abf5f806364332377a89dceed87866d9892052e
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-10-07 22:04:29 -0500

    feat: info component

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const Search = () => {
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get('q'))
   return <div>Search</div>
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,15 @@ module.exports = {
   theme: {
     colors: {
       primary: '#3B0D11',
-      white: '#ffffff'
+      white: '#ffffff',
+      secondary: '#748386',
+      black: {
+        100: '#0D0B0B',
+        75: '#494848',
+        50: '#868585',
+        25: '#C2C2C2',
+        10: '#E7E7E7'
+      }
     },
     extend: {
       fontFamily: {
