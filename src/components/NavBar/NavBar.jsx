b7a48aa42721d9ac5027fbe8cf38add5e626c4ca
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-10-12 20:19:40 -0500

    fix: styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,42 +1,48 @@
-import { useState } from "react";
-import { Links } from "../../models/index.js";
-import { CloseMenu } from "../Icons/CloseMenu.jsx";
-import { HamburgerMenu } from "../Icons/HamburgerMenu.jsx";
-import { NavLink } from "react-router-dom";
-import Info from "../Info/Info.jsx";
+import { useState } from 'react'
+import { Links } from '../../models/index.js'
+import { CloseMenu } from '../Icons/CloseMenu.jsx'
+import { HamburgerMenu } from '../Icons/HamburgerMenu.jsx'
+import { NavLink } from 'react-router-dom'
+import Info from '../Info/Info.jsx'
 
 export const NavBar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <header className="flex w-full flex-col gap-4">
+    <header className='flex w-full flex-col gap-4'>
       {/* Contenedor Menu */}
-      <section className="relative flex h-[4.8rem] w-full items-center justify-center bg-primary px-[1rem] dk:h-[6.3rem] dk:justify-center">
+      <section className='relative flex h-[4.8rem] w-full items-center justify-center bg-primary px-[1rem] dk:h-[6.3rem] dk:justify-center'>
         {/* Icono Menu Hamburger */}
         <div
-          className="absolute left-0 z-10 mx-4 w-[1.35rem] cursor-pointer dk:hidden"
+          className='absolute left-0 z-10 mx-4 w-[1.35rem] cursor-pointer lg:hidden'
           onClick={() => setIsOpen(!isOpen)}
         >
           {isOpen ? <CloseMenu /> : <HamburgerMenu />}
         </div>
-        <h1 className="z-10 font-lora text-[35px] font-bold text-white dk:text-[50px]">
+        <h1 className='z-10 font-lora text-[35px] font-bold text-white dk:text-[50px]'>
           NewsApp
         </h1>
       </section>
       <nav
         className={`fixed bottom-0 top-0 mx-auto flex w-[65%] max-w-screen-dk items-center justify-center bg-primary duration-500 ease-out ${
-          isOpen ? "left-0" : "left-[-100%]"
-        } dk:relative dk:left-0 dk:w-full dk:bg-white dk:bg-none dk:px-4`}
+          isOpen ? 'left-0' : 'left-[-100%]'
+        } lg:relative lg:left-0 lg:w-11/12 lg:bg-white lg:bg-none lg:px-4`}
       >
-        <ul className="flex w-[60%] flex-col gap-8 dk:w-full dk:flex-row dk:justify-between">
+        <ul className='flex w-[60%] flex-col gap-8 lg:w-full lg:flex-row lg:justify-between'>
           {Links.map((link, i) => (
-            <li key={i} onClick={() => setIsOpen(false)} className="">
-              <NavLink to={link.link} className="flex justify-between">
+            <li
+              key={i}
+              onClick={() => setIsOpen(false)}
+            >
+              <NavLink
+                to={link.link}
+                className='flex justify-between'
+              >
                 {({ isActive }) => (
                   <span
-                    className={`w-full text-white dk:px-0 dk:text-[22px] dk:font-bold ${
+                    className={`w-full text-white lg:px-0 lg:text-[22px] lg:font-bold ${
                       isActive
-                        ? "border-r-[0.2rem] border-white dk:border-none dk:text-primary"
-                        : " dk:text-secondary"
+                        ? 'border-r-[0.2rem] border-white lg:border-none lg:text-primary'
+                        : ' transition duration-300 hover:text-primary lg:text-secondary'
                     }`}
                   >
                     {link.name}
@@ -48,9 +54,9 @@ export const NavBar = () => {
         </ul>
       </nav>
       {/* Info Local */}
-      <div className="mx-auto w-full max-w-screen-dk px-4 md:mx-auto md:w-11/12">
+      <div className='mx-auto w-full max-w-screen-dk px-4 md:w-11/12'>
         <Info />
       </div>
     </header>
-  );
-};
+  )
+}
