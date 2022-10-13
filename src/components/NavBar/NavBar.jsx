79388c28f0f32b3e734095a0f5e8ad4f6c8925e7
santiago23-2003 <santiag636@gmail.com>
2022-10-12 19:06:25 -0500

    feat: navbar component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,20 +10,20 @@ export const NavBar = () => {
   return (
     <header className="flex w-full flex-col gap-4">
       {/* Contenedor Menu */}
-      <section className="flex h-[4.8rem] w-full flex-row flex-wrap content-center gap-[4.68rem] bg-primary px-[1rem] dk:h-[6.3rem] dk:justify-center dk:gap-0">
+      <section className="relative flex h-[4.8rem] w-full items-center justify-center bg-primary px-[1rem] dk:h-[6.3rem] dk:justify-center">
         {/* Icono Menu Hamburger */}
         <div
-          className="z-10 w-[1.35rem] cursor-pointer dk:hidden"
+          className="absolute left-0 z-10 mx-4 h-full w-[1.35rem] cursor-pointer dk:hidden"
           onClick={() => setIsOpen(!isOpen)}
         >
           {isOpen ? <CloseMenu /> : <HamburgerMenu />}
         </div>
-        <h1 className="z-10 font-lora text-[35px] font-bold text-white dk:text-[50px]">
+        <div className="z-10 font-lora text-[35px] font-bold text-white dk:text-[50px]">
           NewsApp
-        </h1>
+        </div>
       </section>
       <nav
-        className={`fixed bottom-0 top-0 flex w-[65%] items-center justify-center bg-primary duration-500 ease-out ${
+        className={`fixed bottom-0 top-0 flex w-[65%] items-center justify-center bg-primary/95 duration-500 ease-out ${
           isOpen ? "left-0" : "left-[-100%]"
         } dk:relative dk:left-0 dk:w-full dk:bg-white dk:bg-none dk:px-4`}
       >
@@ -32,13 +32,15 @@ export const NavBar = () => {
             <li
               key={i}
               onClick={() => setIsOpen(false)}
-              className="flex justify-between text-white hover:border-r-[0.2rem] hover:border-white dk:border-none"
+              className="flex justify-between text-white"
             >
               <NavLink to={link.link}>
                 {({ isActive }) => (
                   <span
-                    className={`dk:text-[22px] dk:font-bold ${
-                      isActive ? "dk:text-primary" : "dk:text-secondary"
+                    className={`px-16 dk:px-0 dk:text-[22px] dk:font-bold ${
+                      isActive
+                        ? "border-r-[0.2rem] border-white dk:border-none dk:text-primary"
+                        : "dk:text-secondary"
                     }`}
                   >
                     {link.name}
